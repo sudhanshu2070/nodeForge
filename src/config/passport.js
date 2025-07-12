8daebeb767425ba4fb14e251b8397459d656c75d
sudhanshu2070 <sssallen2016@gmail.com>
2025-07-12 23:46:23 +0530

    Mongo db setup 1.3

--- src/config/passport.js
+++ src/config/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/user.model');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/api/auth/google/callback'
+  callbackURL: process.env.GOOGLE_CALLBACK_URL || '/api/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ googleId: profile.id });
