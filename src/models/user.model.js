fae0cd32ba4c6629091c5524e00dbeeb130f27ce
sudhanshu2070 <sssallen2016@gmail.com>
2025-07-15 01:06:07 +0530

    2FA(Factor Authentication) with SendGrid 0.2

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -42,8 +42,31 @@ const userSchema = new mongoose.Schema({
 
 // Password hashing middleware
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+
+  // Generating custom userId for the user
+  if (!this.userId && this.name) {
+    const namePart = this.name.trim().split(' ')[0]; // First name
+    const regex = new RegExp(`^${namePart}`, 'i');
+
+    const latestUser = await this.constructor
+      .findOne({ userId: { $regex: regex } })
+      .sort({ createdAt: -1 });
+
+    let suffix = '0001';
+    if (latestUser && latestUser.userId) {
+      const match = latestUser.userId.match(/\d+$/);
+      if (match) {
+        const num = parseInt(match[0]) + 1;
+        suffix = num.toString().padStart(4, '0');
+      }
+    }
+
+    this.userId = `${namePart}${suffix}`;
+  }
+
   next();
 });
 
