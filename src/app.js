4bd0cae5b0f53acdbe1d83af041d03c75d8910dd
sudhanshu2070 <sssallen2016@gmail.com>
2025-08-01 02:39:03 +0530

    Email verification BACKEND 0.5

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
+const https = require('https');
+const fs = require('fs');
+const path = require('path');
+
 require('dotenv').config({ path: '/home/ec2-user/nodeForge/.env' });// for ec2 instance deployment
 require('dotenv').config();
+
 const express = require('express');
 const passport = require('passport');
 const cors = require('cors');
@@ -18,18 +23,6 @@ const app = express();
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 
-// // Convert AWS buffer body to string (if needed)
-// app.use((req, res, next) => {
-//   if (Buffer.isBuffer(req.body)) {
-//     try {
-//       req.body = JSON.parse(req.body.toString('utf8'));
-//     } catch (e) {
-//       console.error('Invalid JSON body:', e);
-//     }
-//   }
-//   next();
-// });
-
 app.use(express.json());
 app.use(passport.initialize());
 // app.use(passport.session());
@@ -44,10 +37,18 @@ app.use('/api/auth', require('./routes/auth.routes'));
 app.use('/api/auth', require('./routes/google.routes'));
 
 // Start server
-
 if (process.env.NODE_ENV !== 'serverless') {
   const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => console.log(`Auth service running on port ${PORT}`));
+  // app.listen(PORT, () => console.log(`Auth service running on port ${PORT}`));
+  
+  const sslOptions = {
+    key: fs.readFileSync(path.join(__dirname, 'ssl/key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'ssl/cert.pem')),
+  };
+
+  https.createServer(sslOptions, app).listen(PORT, () => {
+    console.log(`HTTPS server running at :${PORT}`);
+  });
 }
 
 module.exports = app; // Exporting the app for serverless deployment
